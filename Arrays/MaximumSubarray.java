8af6e1bb0d4d192defb3f88ae0a281fa5c07b013
NirmalsaiswaroopJ <128392324+NirmalsaiswaroopJ@users.noreply.github.com>
2024-06-15 21:50:54 +0530

    Add files via upload

---
+++ Arrays/MaximumSubarray.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Main {
+    public static int MaximumSubarray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int currentSum = 0;
+            for (int j = i; j < nums.length; j++) {
+                currentSum += nums[j];
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+                }
+            }
+        }
+        return maxSum;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        int maxSubarraySum = MaximumSubarray(nums);
+        System.out.println("Maximum subarray sum: " + maxSubarraySum);
+    }
+}
