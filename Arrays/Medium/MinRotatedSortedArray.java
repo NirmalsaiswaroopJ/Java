a7a2db932decca19bde8e4e9adc23ea346866652
NirmalsaiswaroopJ <128392324+NirmalsaiswaroopJ@users.noreply.github.com>
2024-06-27 07:03:47 +0530

    Create MinRotatedSortedArray.java

---
+++ Arrays/Medium/MinRotatedSortedArray.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static int findMin(int[] nums){
+        int l = 0;
+        int r = nums.length - 1;
+        int ans = Integer.MAX_VALUE;
+        while(l<=r){
+            int m = (l+r)/2;
+            if(nums[l] <= nums[m]){
+                ans = Math.min(ans, nums[l]);
+                l = m+1;
+            }else {
+                ans = Math.min(ans, nums[m]);
+                r = m -1;
+            }
+        }
+        return ans;
+    }
+}
