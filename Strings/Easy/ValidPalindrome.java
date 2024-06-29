ea3259a5c219e4a04e62b8c6e3ce2318ac793c1b
NirmalsaiswaroopJ <128392324+NirmalsaiswaroopJ@users.noreply.github.com>
2024-06-29 21:44:00 +0530

    Create ValidPalindrome.java

---
+++ Strings/Easy/ValidPalindrome.java
@@ -0,0 +1,21 @@
+public class Main{
+    public boolean static ValidPalindrome(String s){
+        String fixed_str = "";
+        for(char c: s.toCharArray()){
+            if(Character.isDigit(c) || Character.isLetter(c)){
+                fixed_str += c;
+            }
+        }
+        fixed_str = fixed_str.toLowerCase();
+        int a = 0;
+        int b = fixed_str.length()-1;
+        while(a <= b){
+            if(fixed_str.charAt(a) != fixed_str.charAt(b)){
+                return false;
+            }
+            a = a+1;
+            b = b-1;
+        }
+        return true;
+    }
+}
